042b06090a1abb2a0b3f19b21b3fb8d8fb75f8c9
Andrew Nguyen <firecopyx@gmail.com>
2014-03-09 21:33:44 -0500

    libGDX setup

---
+++ CSopoly/src/com/firecopy/DesktopGame.java
@@ -0,0 +1,10 @@
+package com.firecopy;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+
+public class DesktopGame {
+	public static void main(String[] args){
+		new LwjglApplication(new Game(), "Game", 480, 320);
+	}
+
+}
