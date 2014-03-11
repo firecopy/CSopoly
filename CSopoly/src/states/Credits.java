a37fe709e3360f4a4159037e0bb03f4c028e15b2
Andrew Nguyen <firecopyx@gmail.com>
2014-03-11 17:14:57 -0500

    Auto-generated Credits class

--- CSopoly/src/states/Credits.java
+++ CSopoly/src/states/Credits.java
@@ -1,5 +1,36 @@
 package states;
 
-public class Credits {
+import handlers.GameStateManager;
+
+public class Credits extends GameState {
+
+	protected Credits(GameStateManager gsm) {
+		super(gsm);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void handleInput() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(float dt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
